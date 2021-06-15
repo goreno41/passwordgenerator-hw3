9a32262e9a67c36183183057ed7fb6c1aa95264f
Josh Renaud <josh.renaud41@gmail.com>
2021-06-15 10:01:40 -0500

    added arrays for character options.

---
+++ script.js
@@ -0,0 +1,28 @@
+//.. Character arrays for password options
+
+var alphaUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var alphaLowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var specialChar = "!@#$%^&\()*+,-./:;<=>?[\\]_`{|}~";
+var alphaLowercase = alphaLowercase.split ("");
+var alphaUppercase = alphaUppercase.split ("");
+var numbers = numbers.split ("");
+var specialChar = specialChar.split ("");
+
+//.. Function to generate password
+
+function passwordGenerate (){
+    var charactersAll = [];
+    var randomPassword = "";
+}
+
+//.. Set length varibles
+
+
+
+
+//.. Confirmation of options for password
+
+
+
+//.. Display password
